c82ae3cd2d5d3953c296d2a97b3d54224d70fd0e
jaskarnvir singh <jassingh242002@gmail.com>
2026-02-05 17:58:27 +0530

    build: Update API base URL and configure iOS project with CocoaPods.

--- flutter_app/lib/utils/constants.dart
+++ flutter_app/lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   static const String baseUrl =
-      'https://cvczd-34-172-64-198.a.free.pinggy.link'; // HTTPS Tunnel
+      'https://gazebo-simulation-540735931660.us-central1.run.app';
   static const String loginEndpoint = '/token';
   static const String registerEndpoint = '/register';
   static const String userInfoEndpoint = '/users/me';
